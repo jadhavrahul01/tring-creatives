ec9bb7d9fd4730737b35d2742f707177f850cf45
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2025-08-14 20:04:56 +0530

    update mail

--- mail.php
+++ mail.php
@@ -114,7 +114,7 @@ try {
     $mail->addReplyTo($email, $name); // User can reply directly to the sender
 
     // Recipient
-    $mail->addAddress('rohit@tringcreatives.com', 'Tring Creatives Team');
+    $mail->addAddress('adarshtechdev@gmail.com', 'Tring Creatives Team');
 
     // Email content
     $mail->isHTML(true);
